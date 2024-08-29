626e55543f8bdc148f42596ef56677d94a73ddb0
developmentE2B <development@innovatronics.com.mx>
2024-08-28 19:45:49 -0600

    Add files via upload

---
+++ js/about.76a79ed2.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunktest_deploy"]=self["webpackChunktest_deploy"]||[]).push([[594],{603:function(t,e,n){n.r(e),n.d(e,{default:function(){return d}});var u=n(6768);const s={class:"about"},a=(0,u.Lk)("h1",null,"This is an about page",-1),r=[a];function c(t,e){return(0,u.uX)(),(0,u.CE)("div",s,r)}var o=n(1241);const l={},i=(0,o.A)(l,[["render",c]]);var d=i}}]);
+//# sourceMappingURL=about.76a79ed2.js.map
